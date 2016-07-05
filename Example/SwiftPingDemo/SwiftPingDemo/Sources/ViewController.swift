6594056c95988747072e4777543523292868dc2c
Ankit Thakur <ankitthakur85@gmail.com>
2016-07-05 17:21:18 +0530

    Updated example

--- Example/SwiftPingDemo/SwiftPingDemo/Sources/ViewController.swift
+++ Example/SwiftPingDemo/SwiftPingDemo/Sources/ViewController.swift
@@ -5,7 +5,19 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = UIColor.whiteColor()
+    view.backgroundColor = UIColor.white()
   }
+
+	func verifyPing() {
+
+		let config:PingConfiguration = PingConfiguration(interval: 1)
+
+		SwiftPing.ping(host: "google.com",
+		               configuration: config, queue: DispatchQueue.main) { (ping, error) in
+										print(ping)
+										print(error)
+		}
+	}
+	
 }
 
